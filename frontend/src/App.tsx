04e8b35d9adca0b625aaf38b30113393f5d34370
Daniel James <danielcolinjames@gmail.com>
2021-11-14 23:17:45 -0800

    Add Tailwind, reset styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
 import React, { useContext } from "react";
+import "./index.css";
 import "./App.css";
-import { Symfoni, SignerContext, TokenContext } from "./hardhat/SymfoniContext";
+import { Symfoni, SignerContext } from "./hardhat/SymfoniContext";
 import { Greeter } from "./components/Greeter";
 import { BorrowFromVic } from "./components/BorrowFromVic";
-import { VicsClaim } from "./components/VicsClaim";
 import Step1 from "./components/Step1";
 import Step2 from "./components/Step2";
 import Step3 from "./components/Step3";
@@ -14,35 +14,24 @@ import { ethers } from "ethers";
 const provider = new ethers.providers.JsonRpcProvider();
 
 function App() {
-  /* HINT: this is how to bring in the signerContext */
-  const [signer] = useContext(SignerContext);
-
   return (
-    <div className="App">
+    <div className="w-full bg-gray-100">
       <header className="App-header">
         <Symfoni autoInit={true}>
-          <div style={{ width: "75%" }}>
+          <div>
             <div>
-              <p style={{ fontSize: "32px" }}>Yield: UI Technical Assessment</p>
+              <p>Yield: UI Technical Assessment</p>
             </div>
 
             <div className="section">
               <p>Briefing:</p>
               <p>
                 This assessment requires you to create{" "}
-                <a
-                  style={{ color: "white" }}
-                  href="https://reactjs.org/"
-                  target="_blank"
-                >
-                  React
+                <a href="https://reactjs.org/" target="_blank">
+                  React{" "}
                 </a>{" "}
                 elements that interact with the{" "}
-                <a
-                  style={{ color: "white" }}
-                  href="https://ethereum.org/en/"
-                  target="_blank"
-                >
+                <a href="https://ethereum.org/en/" target="_blank">
                   Ethereum
                 </a>{" "}
                 blockchain. We know that some of this may be new to you, and
@@ -53,34 +42,22 @@ function App() {
                 this single page app should connect with a local blockchain
                 implementation, receive and send ETH, and interact with a
                 predeployed{" "}
-                <a
-                  style={{ color: "white" }}
-                  href="https://eips.ethereum.org/EIPS/eip-20"
-                  target="_blank"
-                >
+                <a href="https://eips.ethereum.org/EIPS/eip-20" target="_blank">
                   ERC20
                 </a>{" "}
                 contract, TST.
               </p>
-              <p style={{ fontSize: "14px" }}>
+              <p>
                 A{" "}
-                <a
-                  style={{ color: "white" }}
-                  href="https://hardhat.org/"
-                  target="_blank"
-                >
+                <a href="https://hardhat.org/" target="_blank">
                   Hardhat
                 </a>{" "}
                 implementation is 'pre-setup'. Run it in the background with
                 with: <code> npx hardhat node --watch </code>.
               </p>
-              <p style={{ fontSize: "14px" }}>
+              <p>
                 Also, make sure you have{" "}
-                <a
-                  style={{ color: "white" }}
-                  href="https://metamask.io/"
-                  target="_blank"
-                >
+                <a href="https://metamask.io/" target="_blank">
                   Metamask
                 </a>{" "}
                 installed in your browser.
@@ -90,26 +67,26 @@ function App() {
                 <BorrowFromVic />
               </div>
 
-              <p style={{ fontSize: "14px" }}>
+              <p>
                 If all is running fine at this point, the above button will
                 short-circuit the system and credit the connected metamask
                 account with <strong>1.5ETH</strong>. This should help you get
                 going with the rest of the tasks.
               </p>
-              <p style={{ fontSize: "14px" }}>
+              <p>
                 HINT: Make sure metamask is connected to the 'custom RPC
                 network' with port 31337.
               </p>
             </div>
 
             <ol>
-              <div className="section">
+              <div className="">
                 Working with ETH:
                 <Step1 provider={provider} />
                 <Step2 provider={provider} />
               </div>
 
-              <div className="section">
+              <div className="">
                 Working with ERC20 Tokens:
                 <Step3 provider={provider} />
                 <Step4 provider={provider} />
@@ -117,27 +94,22 @@ function App() {
                 <Step6 provider={provider} />
               </div>
 
-              <div className="section">
+              <div className="">
                 In your comfort zone:
                 <p>
-                  {" "}
                   This app is NOT particularily well made (and the storyline is
                   shoddy). Use whatever tools/packages you feel comfortable with
                   to shuffle things around, tidy things up, or show-off your
-                  skills.{" "}
+                  skills.
                 </p>
                 <li>
-                  {" "}
                   <p>
-                    {" "}
                     <strong>Optional:</strong> Make this page{" "}
                     <strong>nicer to look at</strong>{" "}
                   </p>
                 </li>
                 <li>
-                  {" "}
                   <p>
-                    {" "}
                     <strong>Optional:</strong> Make this test{" "}
                     <strong> nicer to work with</strong> for the next potential
                     candidate
@@ -146,16 +118,16 @@ function App() {
               </div>
             </ol>
 
-            <div className="section">
+            <div className="">
               <p>When you feel like you have done, submit a PR. </p>
-              <p style={{ fontSize: "14px" }}>
+              <p>
                 Thank you very much for taking this assessment. We appreciate
                 your time, and welcome any feedback!
               </p>
             </div>
 
-            <div className="section">
-              <p style={{ fontSize: "14px" }}>
+            <div className="">
+              <p>
                 Here is an extra example of a contract, for reference purposes:
               </p>
               <Greeter></Greeter>
